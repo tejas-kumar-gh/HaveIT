284c4806cbaab5fc8567afa9ad62ac725a078365
tejas-kumar-gh <dhimantejas1234@gmail.com>
2026-02-01 23:19:08 +0530

    changes in packages.json

--- backend/server.js
+++ backend/server.js
@@ -32,7 +32,7 @@ app.get("/", (req, res) => {
   res.send("welcome to our home page of site");
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`http://localhost:3000`);
 });
