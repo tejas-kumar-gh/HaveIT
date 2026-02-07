04877c51f6b97cb63f12b705724d4afcd1841ba0
tejas-kumar-gh <dhimantejas1234@gmail.com>
2026-02-08 00:13:38 +0530

    changes in the .env file in backend

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -49,7 +49,7 @@ const login = async (req, res) => {
 
     res.cookie("token", token, {
       httpOnly: true,
-      secure: true,
+      secure: false,
       sameSite: "none",
       maxAge: 24 * 60 * 60 * 1000
     });
